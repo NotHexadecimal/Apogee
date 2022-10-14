1c1f880cc1c791dbdfb689919296f9a51cd389ac
Kevin <kevin.fontanari@gmail.com>
2022-10-15 00:40:34 +0200

    Added canvas and resizing mechanism.

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,16 @@
-console.log('Henlo, world!')
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const resizeDraw = () => {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(canvas.width, canvas.height);
+  ctx.stroke();
+}
+
+addEventListener('resize', resizeDraw);
+
+resizeDraw();
